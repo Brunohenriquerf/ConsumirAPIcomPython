f16ddc5fb82ecea2834e3f9d1eb7dd20d872042a
Bruno Fernandes <91612856+Brunohenriquerf@users.noreply.github.com>
2022-01-01 23:00:29 -0300

    Update Consumir API.py

--- Consumir API.py
+++ Consumir API.py
@@ -5,8 +5,9 @@ url = "https://jsonplaceholder.typicode.com/todos/15"
 response = requests.get(url)
 print("Antigo: ", response.json())
 # Atualizar valores
-minha_lista = {"userId": 13, "title": "Pudim de frango", "completed": True}
+minha_lista = {"userId": 13, "id": 13, "title": "Pudim de frango com barbecue", "completed": True}
 response = requests.put(url, json=minha_lista)
-print("Código: ", response.status_code) # Documentação, https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+# Documentação https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+print("Deu certo, segue abaixo a informação atualizada:") if response.status_code == 200 else print("Algo deu errado, a informação não foi alterada:")
 # Visualizar novamente, para checar a atualização
 print("Novo: " ,response.json())
